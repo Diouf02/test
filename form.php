9afe336809d8b63bd9656b332acd494b9cf09be7
Diouf02 <cheikhlegatois@gmail.com>
2024-09-11 16:53:17 +0100

    Second commit

---
+++ form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="post" action="index.php">
+    <label for="nom">Nom :</label>
+    <input type="text" id="nom" name="nom">
+    <input type="submit" value="Envoyer">
+</form>
+<?php 
+
+$var=rand(1,16);
+?>
+</body>
+</html>
+
+
